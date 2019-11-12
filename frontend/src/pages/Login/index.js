41fd87eb72a72e3d5f5d4582b4ffd8ce0f16789a
unknown <bruna.fernandez@mjv.com.br>
2019-11-12 11:59:27 -0300

    changing colors and deleting extra content

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -53,63 +53,9 @@ class Login extends React.Component {
           </div>
         </div>
       </div>
-        
-        {/* <div className="center">  
-        <div className = "title">
-          <h1> LOGIN ON <span>post</span> </h1> 
-        </div> 
-        <div className = "box">
-            <form className = "formLogin" onSubmit = {this.handleSubmit}>
-            <input type = "email" id = "email" placeholder = "E-mail" onChange = {event => this.setState({email: event.target.value})} /> 
-
-            <input type = "password" id = "password" placeholder = "Senha" autoComplete = "password" value = {this.state.password} onChange = {event => this.setState({password: event.target.value})}/> 
-
-            <div className = "savePassword">
-              <input type = "checkbox" id = "checkbox" />
-              <label className = "remember" > Lembrar minha senha </label>  
-            </div>
-              <button className = "btn" type = "submit" > Entrar </button> 
-              <label> {this.state.texto} </label>  
-          </form>
-        </div>
-      </div>     */}
       </>    
     )
   }
 }
 
 export default Login;
-
-          {/* <div className="backG">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12 main">
-                <h1 className="mainTitle"> Bem vindo ao Post!</h1>
-              </div>
-            </div>  
-          </div>  
-        </div>  */}
-
-{/* 
-         <div class="navbar">
-          <h1 className="PostLogo">Post</h1>
-        </div>
-        <div className="bImage">
-          <div className="contLogin">
-            <img className="left" src={leftBg} alt="left bracket"/>
-            <form className= "formLogin" onSubmit = {this.handleSubmit}>
-              <div className="typewriter">
-                <h1>LOGIN</h1>
-              </div>
-              <input type = "email" id = "email" placeholder = "E-mail"  onChange = {event => this.setState({email: event.target.value})} />
-              <input type="password" id="password" placeholder="Senha" autoComplete="password" value={this.state.password} onChange={event => this.setState({password: event.target.value})} />
-              <div className="savePassword">
-                <input type="checkbox" id = "checkbox" />
-                <label className="remember"> Lembrar minha senha </label> 
-              </div>
-              <button className="btn" type="submit"> Entrar </button> 
-              <label> {this.state.texto} </label> 
-            </form>
-            <img className="right" src={rightBg} alt="right bracket" />
-          </div>  
-        </div>  */} 
