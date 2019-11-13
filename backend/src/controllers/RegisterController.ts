9a8f1c9c416424a75daf30e47487f4617de2bf8a
PauloLuedy <pauloluedyads@gmail.com>
2019-11-12 21:37:23 -0300

    registrar usuario com Criptografia

--- backend/src/controllers/RegisterController.ts
+++ backend/src/controllers/RegisterController.ts
@@ -1,35 +1,36 @@
-// import User from "../models/User";
-// import * as bcrypt from "bcryptjs";
+/*  import User from "../models/User";
+ import * as bcrypt from "bcryptjs";
 
-// const salt = 5;
-// class RegisterController {
-//   async store(req: any, res: any) {
-//     const { name, email, password } = req.body;
-//     let user = await User.findOne({ email });
+ const salt = 5;
+ class RegisterController {
+   async store(req: any, res: any) {
+     const { name, email, password } = req.body;
+     let user = await User.findOne({ email });
 
-//     if (name && email && password) {
-//       if (user) {
-//         return res.send("Usuario já cadastrado");
-//       } else {
-//         await bcrypt.hash(req.body.password, salt, function(_, hash) {
-//           User.create({
-//             name,
-//             email,
-//             password: hash
-//           }).then(
-//             function() {
-//               return res.redirect("/login");
-//             },
-//             function() {
-//               return res.send("Erro ao cadastrar Usuario");
-//             }
-//           );
-//         });
-//       }
-//     } else {
-//       return res.send("Preencha todos os campos");
-//     }
-//   }
-// }
+     if (name && email && password) {
+       if (user) {
+         return res.send("Usuario já cadastrado");
+       } else {
+         await bcrypt.hash(req.body.password, salt, function(_, hash) {
+           User.create({
+             name,
+             email,
+             password: hash
+           }).then(
+             function() {
+               return res.redirect("/login");
+             },
+             function() {
+               return res.send("Erro ao cadastrar Usuario");
+             }
+           );
+         });
+       }
+     } else {
+       return res.send("Preencha todos os campos");
+     }
+   }
+ }
 
-// export default new RegisterController();
+ export default new RegisterController();
+ */
